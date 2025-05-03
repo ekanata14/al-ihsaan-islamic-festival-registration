cae8a745104b1a643e774cb7cf297b7bb6168c1a
ekanata14 <ekanata1411@gmail.com>
2025-05-03 19:13:29 +0800

    feat: open login & register button

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -61,10 +61,10 @@
                         <a href="#contact-us"
                             class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact Us</a>
                     </li>
-                    {{-- <li class="flex gap-2">
+                    <li class="flex gap-2">
                         <a href="{{ route('login') }}" class="btn-primary">Login</a>
                         <a href="{{ route('register') }}" class="btn-green">Daftar</a>
-                    </li> --}}
+                    </li>
                 </ul>
             </div>
             <script>
