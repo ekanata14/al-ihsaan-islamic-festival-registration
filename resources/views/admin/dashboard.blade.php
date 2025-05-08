df628a7b6e9b1612ac3b4e908babf0b3d3b5f410
ekanata14 <ekanata1411@gmail.com>
2025-05-08 21:00:29 +0800

    feat: navigation link on dashboard admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -78,7 +78,7 @@
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Total Pendaftaran Khitan</h5>
                     </a>
                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $totalRegistrationKhitans }}</p>
-                    <a href="{{ route('admin.dashboard.registration') }}"
+                    <a href="{{ route('admin.dashboard.khitan-registration') }}"
                         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Detail
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
