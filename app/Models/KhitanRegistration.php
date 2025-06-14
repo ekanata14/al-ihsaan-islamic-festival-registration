f6495f634eb4c7bd3b8c41c8dc789d70cef9865b
ekanata14 <ekanata1411@gmail.com>
2025-06-14 22:40:22 +0800

    feat: update for khitan

--- app/Models/KhitanRegistration.php
+++ app/Models/KhitanRegistration.php
@@ -51,4 +51,14 @@ class KhitanRegistration extends Model
     {
         return $this->hasOne(KhitanFamilyCard::class);
     }
+
+    public function participants()
+    {
+        return $this->hasMany(Participant::class);
+    }
+
+    public function participant()
+    {
+        return $this->hasOne(Participant::class);
+    }
 }
